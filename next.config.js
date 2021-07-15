cb6223eaafe52d85b960f9365bb71d35907725a0
gallegoagustin <gallegoagustin7@gmail.com>
2021-07-14 23:29:16 -0300

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 require('./server/database.js');
 module.exports = {
   images: {
-    domains: ['cloudinary.com', 'amazonaws.com'],
+    domains: ['cloudinary.com', 'amazonaws.com', 'https://ecommics.s3.sa-east-1.amazonaws.com/images'],
     loader: 'cloudinary',
   },
   reactStrictMode: true,
